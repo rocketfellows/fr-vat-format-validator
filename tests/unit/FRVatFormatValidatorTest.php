4d433095619793c45c5d0d254c9fd2985c1fde32
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-03-07 23:47:10 +0400

    [CVFV-5] setup test.

--- tests/unit/FRVatFormatValidatorTest.php
+++ tests/unit/FRVatFormatValidatorTest.php
@@ -1,22 +1,21 @@
 <?php
 
-// TODO: set namespace
-namespace unit;
+namespace rocketfellows\FRVatFormatValidator\tests\unit;
 
-class CountryVatFormatValidatorTest extends TestCase
+use PHPUnit\Framework\TestCase;
+
+class FRVatFormatValidatorTest extends TestCase
 {
     /**
-     * TODO: set type
-     * @var
+     * @var FRVatFormatValidator
      */
     private $validator;
 
-    /**
-     * TODO: setup validator for test
-     */
     protected function setUp(): void
     {
-        $this->validator = new CountryVatFormatValidator();
+        parent::setUp();
+
+        $this->validator = new FRVatFormatValidator();
     }
 
     /**
