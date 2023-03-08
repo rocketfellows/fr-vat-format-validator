d4382c4663ee134ec59100bb1be70d2f30735e84
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-03-08 18:12:15 +0400

    [CVFV-5] injected validator.

--- tests/unit/FRVatFormatValidatorTest.php
+++ tests/unit/FRVatFormatValidatorTest.php
@@ -3,6 +3,7 @@
 namespace rocketfellows\FRVatFormatValidator\tests\unit;
 
 use PHPUnit\Framework\TestCase;
+use rocketfellows\FRVatFormatValidator\FRVatFormatValidator;
 
 class FRVatFormatValidatorTest extends TestCase
 {
